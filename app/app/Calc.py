35c190668b21dd022796678e650082a911b71354
26518304542 <ozgurcimen1@gmail.com>
2024-10-01 12:41:29 +0300

    "wait for db" 

---
+++ app/app/Calc.py
@@ -0,0 +1,6 @@
+"""
+    Calc
+"""
+
+def add(x,y):
+    return x+y    
---
+++ app/app/tests.py
@@ -0,0 +1,10 @@
+from django.test import SimpleTestCase
+
+from app import Calc
+
+
+class CalcTests(SimpleTestCase):
+
+    def test_add_numbers(self):
+        res = Calc.add(5,6)
+        self.assertEqual(res, 11)
